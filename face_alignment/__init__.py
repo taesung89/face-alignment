881c3390f26704f942ad93f34108df5e94b33d52
Taesung Park <taesung89@gmail.com>
2018-04-03 13:46:23 -0700

    exported more apis

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -5,3 +5,5 @@ __email__ = 'adrian.bulat@nottingham.ac.uk'
 __version__ = '0.1.0'
 
 from .api import FaceAlignment, LandmarksType, NetworkSize
+from .models import FAN
+from .utils import get_preds_fromhm, crop
